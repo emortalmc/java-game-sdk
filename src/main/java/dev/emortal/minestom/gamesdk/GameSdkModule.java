e1245221947c3aa3ce823fac4ddff267d97d465e
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2023-01-05 20:49:55 +0000

    Add moduledata

--- src/main/java/dev/emortal/minestom/gamesdk/GameSdkModule.java
+++ src/main/java/dev/emortal/minestom/gamesdk/GameSdkModule.java
@@ -1,12 +1,14 @@
 package dev.emortal.minestom.gamesdk;
 
 import dev.emortal.minestom.core.module.Module;
+import dev.emortal.minestom.core.module.ModuleData;
 import dev.emortal.minestom.core.module.ModuleEnvironment;
 import dev.emortal.minestom.gamesdk.config.GameSdkConfig;
 import dev.emortal.minestom.gamesdk.game.AgonesListener;
 import dev.emortal.minestom.gamesdk.game.GameManager;
 import org.jetbrains.annotations.NotNull;
 
+@ModuleData(name = "game-sdk", required = false)
 public final class GameSdkModule extends Module {
     private static GameSdkConfig config;
     private static GameManager gameManager;
