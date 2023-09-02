8392a1ef85134e65775f997e6b747a35d2f44a73
Callum Seabrook <callum.seabrook@prevarinite.com>
2023-09-02 23:11:29 +0100

    Added builder to game server for more config freedom

--- src/main/java/dev/emortal/minestom/gamesdk/MinestomGameServer.java
+++ src/main/java/dev/emortal/minestom/gamesdk/MinestomGameServer.java
@@ -1,64 +1,41 @@
 package dev.emortal.minestom.gamesdk;
 
-import dev.emortal.api.modules.ModuleProvider;
-import dev.emortal.minestom.core.Environment;
-import dev.emortal.minestom.core.MinestomServer;
-import dev.emortal.minestom.core.module.kubernetes.KubernetesModule;
-import dev.emortal.minestom.core.module.messaging.MessagingModule;
-import dev.emortal.minestom.gamesdk.command.GameSdkCommand;
+import dev.emortal.api.modules.LoadableModule;
+import dev.emortal.api.modules.Module;
 import dev.emortal.minestom.gamesdk.config.GameSdkConfig;
 import dev.emortal.minestom.gamesdk.game.GameProvider;
-import dev.emortal.minestom.gamesdk.internal.AgonesGameListener;
-import dev.emortal.minestom.gamesdk.internal.GameManager;
-import dev.emortal.minestom.gamesdk.internal.listener.AgonesGameUpdateListener;
-import dev.emortal.minestom.gamesdk.internal.listener.GameUpdateListener;
-import java.util.function.Supplier;
-import net.minestom.server.MinecraftServer;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public final class MinestomGameServer {
-    private static final Logger LOGGER = LoggerFactory.getLogger(MinestomGameServer.class);
-    public static final boolean TEST_MODE = !Environment.isProduction() && Boolean.parseBoolean(System.getenv("GAME_SDK_TEST_MODE"));
+import java.util.function.Supplier;
 
-    public static @NotNull MinestomGameServer create(@NotNull Supplier<GameSdkConfig> configSupplier) {
-        MinestomServer server = MinestomServer.builder().commonModules().build();
-        MinestomGameServer gameServer = initGameSdk(server.getModuleManager(), configSupplier.get());
-        server.start();
-        return gameServer;
+public interface MinestomGameServer {
+    boolean TEST_MODE = MinestomGameServerImpl.TEST_MODE;
+
+    static @NotNull Builder builder() {
+        return new MinestomGameServerImpl.BuilderImpl();
     }
 
-    private static @NotNull MinestomGameServer initGameSdk(@NotNull ModuleProvider moduleProvider, @NotNull GameSdkConfig config) {
-        LOGGER.info("Initializing Game SDK (test mode: {}, config: {})", TEST_MODE, config);
+    static @NotNull MinestomGameServer create(@NotNull Supplier<GameSdkConfig> configSupplier) {
+        return builder().configSupplier(configSupplier).build();
+    }
 
-        MessagingModule messaging = moduleProvider.getModule(MessagingModule.class);
-        KubernetesModule kubernetesModule = moduleProvider.getModule(KubernetesModule.class);
-        boolean hasAgones = kubernetesModule != null && kubernetesModule.getSdk() != null;
+    @NotNull GameProvider getGameProvider();
 
-        GameUpdateListener updateListener;
-        if (TEST_MODE || !hasAgones) {
-            updateListener = GameUpdateListener.NO_OP;
-        } else {
-            updateListener = new AgonesGameUpdateListener(config, kubernetesModule.getSdk());
+    interface Builder {
+
+        @NotNull Builder address(@NotNull String address);
+
+        @NotNull Builder port(int port);
+
+        @NotNull Builder mojangAuth(boolean mojangAuth);
+
+        @NotNull Builder module(@NotNull Class<? extends Module> type, @NotNull LoadableModule.Creator creator);
+
+        @NotNull Builder.EndStep configSupplier(@NotNull Supplier<GameSdkConfig> configSupplier);
+
+        interface EndStep {
+
+            @NotNull MinestomGameServer build();
         }
-        GameManager gameManager = new GameManager(config, updateListener);
-
-        if (messaging != null) {
-            new AgonesGameListener(gameManager, config, messaging);
-        }
-        MinecraftServer.getCommandManager().register(new GameSdkCommand(gameManager));
-
-        return new MinestomGameServer(gameManager);
-    }
-
-    private final GameProvider gameProvider;
-
-    private MinestomGameServer(@NotNull GameProvider gameProvider) {
-        this.gameProvider = gameProvider;
-    }
-
-    public @NotNull GameProvider getGameProvider() {
-        return this.gameProvider;
     }
 }
---
+++ src/main/java/dev/emortal/minestom/gamesdk/MinestomGameServerImpl.java
@@ -0,0 +1,105 @@
+package dev.emortal.minestom.gamesdk;
+
+import dev.emortal.api.modules.LoadableModule;
+import dev.emortal.api.modules.Module;
+import dev.emortal.api.modules.ModuleProvider;
+import dev.emortal.minestom.core.Environment;
+import dev.emortal.minestom.core.MinestomServer;
+import dev.emortal.minestom.core.module.kubernetes.KubernetesModule;
+import dev.emortal.minestom.core.module.messaging.MessagingModule;
+import dev.emortal.minestom.gamesdk.command.GameSdkCommand;
+import dev.emortal.minestom.gamesdk.config.GameSdkConfig;
+import dev.emortal.minestom.gamesdk.game.GameProvider;
+import dev.emortal.minestom.gamesdk.internal.AgonesGameListener;
+import dev.emortal.minestom.gamesdk.internal.GameManager;
+import dev.emortal.minestom.gamesdk.internal.listener.AgonesGameUpdateListener;
+import dev.emortal.minestom.gamesdk.internal.listener.GameUpdateListener;
+
+import java.util.function.Supplier;
+import net.minestom.server.MinecraftServer;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+final class MinestomGameServerImpl implements MinestomGameServer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MinestomGameServerImpl.class);
+    static final boolean TEST_MODE = !Environment.isProduction() && Boolean.parseBoolean(System.getenv("GAME_SDK_TEST_MODE"));
+
+    private final GameProvider gameProvider;
+
+    private MinestomGameServerImpl(@NotNull GameProvider gameProvider) {
+        this.gameProvider = gameProvider;
+    }
+
+    @Override
+    public @NotNull GameProvider getGameProvider() {
+        return this.gameProvider;
+    }
+
+    static final class BuilderImpl implements Builder, Builder.EndStep {
+
+        private final MinestomServer.Builder serverBuilder = MinestomServer.builder();
+        private Supplier<GameSdkConfig> configSupplier;
+
+        @Override
+        public @NotNull Builder address(@NotNull String address) {
+            this.serverBuilder.address(address);
+            return this;
+        }
+
+        @Override
+        public @NotNull Builder port(int port) {
+            this.serverBuilder.port(port);
+            return this;
+        }
+
+        @Override
+        public @NotNull Builder mojangAuth(boolean mojangAuth) {
+            this.serverBuilder.mojangAuth(mojangAuth);
+            return this;
+        }
+
+        @Override
+        public @NotNull Builder module(@NotNull Class<? extends Module> type, @NotNull LoadableModule.Creator creator) {
+            this.serverBuilder.module(type, creator);
+            return this;
+        }
+
+        @Override
+        public @NotNull EndStep configSupplier(@NotNull Supplier<GameSdkConfig> configSupplier) {
+            this.configSupplier = configSupplier;
+            return this;
+        }
+
+        @Override
+        public @NotNull MinestomGameServerImpl build() {
+            MinestomServer server = this.serverBuilder.build();
+            MinestomGameServerImpl gameServer = initialize(server.getModuleManager(), this.configSupplier.get());
+            server.start();
+            return gameServer;
+        }
+
+        private static @NotNull MinestomGameServerImpl initialize(@NotNull ModuleProvider moduleProvider, @NotNull GameSdkConfig config) {
+            LOGGER.info("Initializing Game SDK (test mode: {}, config: {})", TEST_MODE, config);
+
+            MessagingModule messaging = moduleProvider.getModule(MessagingModule.class);
+            KubernetesModule kubernetesModule = moduleProvider.getModule(KubernetesModule.class);
+            boolean hasAgones = kubernetesModule != null && kubernetesModule.getSdk() != null;
+
+            GameUpdateListener updateListener;
+            if (TEST_MODE || !hasAgones) {
+                updateListener = GameUpdateListener.NO_OP;
+            } else {
+                updateListener = new AgonesGameUpdateListener(config, kubernetesModule.getSdk());
+            }
+            GameManager gameManager = new GameManager(config, updateListener);
+
+            if (messaging != null) {
+                new AgonesGameListener(gameManager, config, messaging);
+            }
+            MinecraftServer.getCommandManager().register(new GameSdkCommand(gameManager));
+
+            return new MinestomGameServerImpl(gameManager);
+        }
+    }
+}
