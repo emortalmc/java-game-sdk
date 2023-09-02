7a39e1795f76e58d33b4ad2e009cb037883345a7
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2023-09-02 19:47:08 +0100

    Little bit of cleanup/race condition fix

--- src/main/java/dev/emortal/minestom/gamesdk/config/GameCreationInfo.java
+++ src/main/java/dev/emortal/minestom/gamesdk/config/GameCreationInfo.java
@@ -4,7 +4,6 @@ import dev.emortal.api.model.matchmaker.Match;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.time.Instant;
 import java.util.Set;
 import java.util.UUID;
 
@@ -12,7 +11,7 @@ import java.util.UUID;
  * The map ID, game mode ID, and player IDs would usually come from the matchmaker, when running in the standard environment.
  * The allocation time comes from the implementation (the game SDK).
  */
-public record GameCreationInfo(@NotNull Match match, @NotNull Set<UUID> playerIds, @NotNull Instant allocationTime) {
+public record GameCreationInfo(@NotNull Match match, @NotNull Set<UUID> playerIds) {
 
     public @NotNull String id() {
         return this.match.getId();
--- src/main/java/dev/emortal/minestom/gamesdk/internal/AgonesGameListener.java
+++ src/main/java/dev/emortal/minestom/gamesdk/internal/AgonesGameListener.java
@@ -6,18 +6,17 @@ import dev.emortal.api.model.matchmaker.Match;
 import dev.emortal.api.model.matchmaker.Ticket;
 import dev.emortal.api.utils.kafka.FriendlyKafkaProducer;
 import dev.emortal.minestom.core.module.messaging.MessagingModule;
-import dev.emortal.minestom.gamesdk.game.Game;
 import dev.emortal.minestom.gamesdk.config.GameCreationInfo;
 import dev.emortal.minestom.gamesdk.config.GameSdkConfig;
+import dev.emortal.minestom.gamesdk.game.Game;
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.entity.Player;
+import net.minestom.server.network.ConnectionManager;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
-
-import net.minestom.server.MinecraftServer;
-import net.minestom.server.entity.Player;
-import org.jetbrains.annotations.NotNull;
-
-import java.time.Instant;
 
 public final class AgonesGameListener {
     private static final String GAME_SDK_TOPIC = "game-sdk";
@@ -35,19 +34,17 @@ public final class AgonesGameListener {
     }
 
     private void onMatchCreated(@NotNull Match match) {
-        Instant allocationTime = Instant.now();
-
-        GameCreationInfo creationInfo = this.createInfo(match, allocationTime);
+        GameCreationInfo creationInfo = this.createInfo(match);
         Game game = this.gameManager.createGame(creationInfo);
 
         PreGameInitializer initializer = new PreGameInitializer(this.config, game);
-        initializer.scheduleGameStart();
+        initializer.scheduleGameStart(); // TODO what... why?
 
-        this.notifyGameReady(match);
         this.movePlayersOnThisServer(game, creationInfo.playerIds());
+        this.notifyGameReady(match);
     }
 
-    private @NotNull GameCreationInfo createInfo(@NotNull Match match, @NotNull Instant allocationTime) {
+    private @NotNull GameCreationInfo createInfo(@NotNull Match match) {
         Set<UUID> playerIds = new HashSet<>();
 
         for (Ticket ticket : match.getTicketsList()) {
@@ -56,7 +53,7 @@ public final class AgonesGameListener {
             }
         }
 
-        return new GameCreationInfo(match, playerIds, allocationTime);
+        return new GameCreationInfo(match, playerIds);
     }
 
     private void notifyGameReady(@NotNull Match match) {
@@ -64,8 +61,10 @@ public final class AgonesGameListener {
     }
 
     private void movePlayersOnThisServer(@NotNull Game game, @NotNull Set<UUID> playerIds) {
+        ConnectionManager connectionManager = MinecraftServer.getConnectionManager();
+
         for (UUID playerId : playerIds) {
-            Player player = MinecraftServer.getConnectionManager().getPlayer(playerId);
+            Player player = connectionManager.getPlayer(playerId);
             if (player == null) continue;
 
             Game oldGame = this.gameManager.findGame(player);
