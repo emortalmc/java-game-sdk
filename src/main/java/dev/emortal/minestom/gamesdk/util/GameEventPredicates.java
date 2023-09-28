216bf6c3f84fc8ba3944c4fcf6e807304de5ab65
Callum Seabrook <callum.seabrook@prevarinite.com>
2023-09-28 17:01:00 +0100

    Added module info and remove docs on game event predicates

--- src/main/java/dev/emortal/minestom/gamesdk/util/GameEventPredicates.java
+++ src/main/java/dev/emortal/minestom/gamesdk/util/GameEventPredicates.java
@@ -2,13 +2,11 @@ package dev.emortal.minestom.gamesdk.util;
 
 import dev.emortal.minestom.gamesdk.config.GameCreationInfo;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 import dev.emortal.minestom.gamesdk.game.Game;
 import net.minestom.server.event.Event;
 import net.minestom.server.event.trait.InstanceEvent;
 import net.minestom.server.event.trait.PlayerEvent;
-import net.minestom.server.instance.Instance;
 import org.jetbrains.annotations.NotNull;
 
 public final class GameEventPredicates {
@@ -22,11 +20,6 @@ public final class GameEventPredicates {
         };
     }
 
-    /**
-     * This is for use in game servers, not really in here, which is why it isn't used.
-     *
-     * This is a very common condition for game servers' event nodes.
-     */
     public static @NotNull Predicate<Event> inGame(@NotNull Game game) {
         return event -> {
             if (event instanceof PlayerEvent playerEvent) {
@@ -40,6 +33,5 @@ public final class GameEventPredicates {
     }
 
     private GameEventPredicates() {
-        throw new AssertionError("This class cannot be instantiated.");
     }
 }
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module emortalmc.gamesdk {
+    exports dev.emortal.minestom.gamesdk;
+    exports dev.emortal.minestom.gamesdk.config;
+    exports dev.emortal.minestom.gamesdk.game;
+    exports dev.emortal.minestom.gamesdk.settings;
+    exports dev.emortal.minestom.gamesdk.util;
+}
