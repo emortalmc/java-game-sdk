8385395800021450389deb153070e325c29a418d
Callum Seabrook <callum.seabrook@prevarinite.com>
2023-09-28 17:02:14 +0100

    Remove module info

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module emortalmc.gamesdk {
-    exports dev.emortal.minestom.gamesdk;
-    exports dev.emortal.minestom.gamesdk.config;
-    exports dev.emortal.minestom.gamesdk.game;
-    exports dev.emortal.minestom.gamesdk.settings;
-    exports dev.emortal.minestom.gamesdk.util;
-}
